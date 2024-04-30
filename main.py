c7b730f03ba2618ea2657f363bd9247f0bb0b463
Alex <alexeyrv@users.noreply.github.com>
2024-04-30 03:31:32 +0200

    Update main.py to show difference in my repo

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello my Render.com free account test."}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Optional[str] = "None"):
     return {"item_id": item_id, "q": q}
